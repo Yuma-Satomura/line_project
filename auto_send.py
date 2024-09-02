2a42a6a1b7e8d4ccc34d226e45efb317212c2ecd
yuma.satomura <yuma5136@hotmail.com>
2024-09-02 13:06:05 +0900

    Your commit message

--- auto_send.py
+++ auto_send.py
@@ -1,6 +1,11 @@
+"""
+This module handles the automatic sending of LINE messages based on a schedule.
+"""
+
+import time
+import schedule
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
-import schedule
-import time
 
 #LINE API設定
+line_bot_api=LineBotApi()
